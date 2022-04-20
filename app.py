62012e0af87c8900c1dce9c8bc72bfaed8319f05
DESKTOP-H4UPH14\Benjamín Fernández <bfernandez@inf.udec.cl>
2022-04-19 23:33:29 -0400

    *Titulo encuesta (Solo interfaz) *Integrando tipo de preguntas (En progreso)

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
+@app.route("/createSurvey/<string:section>")
+def index(section):
     return render_template("admin/create_survey.html", navOptions={
         "options": ["Preguntas", "Respuestas", "Configuración"],
-        "selected": "Preguntas"
+        "selected": section
     }
     )
--- static/js/nav.js
+++ static/js/nav.js
@@ -1,12 +1,14 @@
 'use strict'
+console.log(navOptions.selected.toLowerCase())
 
-const opcionNavA = document.querySelector(`a[name=${navOptions.selected}]`);
+const opcionNavA = document.querySelector(`a[name=${navOptions.selected.toLowerCase()}]`);
 const opcionNavDot = document.querySelector(`div[name=${navOptions.selected}]`);
 
-// opcionNavA.style.color = '#000'
-// opcionNavDot.style.background = '#000'
-// opcionNavDot.style.borderColor = '#000'
-opcionNavA.className += " hover"
+opcionNavA.style.color = '#000'
+opcionNavDot.style.background = '#000'
+opcionNavDot.style.borderColor = '#000'
+opcionNavA.className +=' disabled'
+
 
 
 // password.addEventListener('blur', (event) => {

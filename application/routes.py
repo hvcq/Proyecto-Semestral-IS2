deab004dfeab053e3985d67f2c4838d6c34099a1
mellokx <hernanvicentecaro@gmail.com>
2022-04-25 16:06:21 -0400

    Guarda en listas preguntas desarrollo y preguntas alternativa sin opciones

--- application/routes.py
+++ application/routes.py
@@ -1,11 +1,11 @@
 """Rutas de la aplicacion"""
 from datetime import datetime,date
+from operator import length_hint
 
 from flask import current_app as app
 from flask import make_response, redirect, render_template, request, url_for
 
-from .models import Admin, Encuesta, db
-
+from .models import *
 
 @app.route("/")
 def index():
@@ -67,6 +67,50 @@ def Survey(id,section="preguntas"):
             }
         )
     else:
+        ids_preguntas_desarrollo = []
+        numeros_preguntas_desarrollo = []
+        enunciado_preguntas_desarrollo = []
+        comentario_preguntas_desarrollo = []
+        if db.session.query(Desarrollo_Encuesta).filter_by(id_encuesta = id).first() != None:
+            tuplas_desarrollo_encuesta = db.session.query(Desarrollo_Encuesta).filter_by(id_encuesta = id).all()
+            for tupla_desarrollo_encuesta in tuplas_desarrollo_encuesta:
+                ids_preguntas_desarrollo.append(tupla_desarrollo_encuesta.id_pregunta_desarrollo)
+            tuplas_pregunta_desarrollo = db.session.query(Pregunta_Desarrollo).filter(Pregunta_Desarrollo.id_pregunta_desarrollo.in_(ids_preguntas_desarrollo)).all()
+            for tupla_pregunta_desarrollo in tuplas_pregunta_desarrollo:
+                numeros_preguntas_desarrollo.append(tupla_pregunta_desarrollo.numero)
+                enunciado_preguntas_desarrollo.append(tupla_pregunta_desarrollo.enunciado)
+                comentario_preguntas_desarrollo.append(tupla_pregunta_desarrollo.comentario)
+            print("numeros_pd")
+            print(numeros_preguntas_desarrollo)
+            print("enunciados_pd")
+            print(enunciado_preguntas_desarrollo)
+            print("comentarios_pd")
+            print(comentario_preguntas_desarrollo)
+        
+        ids_preguntas_alternativas = []
+        numeros_preguntas_alternativas = []
+        enunciado_preguntas_alternativas = []
+        comentario_preguntas_alternativas = []
+        ids_opciones = []
+        if db.session.query(Alternativa_Encuesta).filter_by(id_encuesta = id).first() != None:
+            tuplas_alternativa_encuesta = db.session.query(Alternativa_Encuesta).filter_by(id_encuesta = id).all()
+            for tupla_alternativa_encuesta in tuplas_alternativa_encuesta:
+                ids_preguntas_alternativas.append(tupla_alternativa_encuesta.id_pregunta_alternativa)
+            tuplas_pregunta_alternativa = db.session.query(Pregunta_Alternativa).filter(Pregunta_Alternativa.id_pregunta_alternativa.in_(ids_preguntas_alternativas)).all()
+            for tupla_pregunta_alternativa in tuplas_pregunta_alternativa:
+                numeros_preguntas_alternativas.append(tupla_pregunta_alternativa.numero)
+                enunciado_preguntas_alternativas.append(tupla_pregunta_alternativa.enunciado)
+                comentario_preguntas_alternativas.append(tupla_pregunta_alternativa.comentario)
+            print("numeros_pa")
+            print(numeros_preguntas_alternativas)
+            print("enunciados_pa")
+            print(enunciado_preguntas_alternativas)
+            print("comentarios_pa")
+            print(comentario_preguntas_alternativas)
+            # tuplas_alternativas = db.session.query(Alternativas).filter(Alternativas.id_pregunta_alternativa.in_(ids_preguntas_alternativas)).all()
+            # for tupla_alternativas in tuplas_alternativas:
+            #     ids_opciones.append(tupla_alternativas.id_opcion)
+            # print(ids_opciones)
         dataSurvey = {
             "id": id,
             "title": db.session.query(Encuesta).filter_by(id_encuesta = id).first().titulo,

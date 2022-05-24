7670b14805ec4b2cb066131f7d5395e19b9b469e
Leonardo Aravena Cuevas <ld.aravena@gmail.com>
2022-05-23 20:15:44 -0400

    Obtención de mails de BD, codificación de links

--- application/mail.py
+++ application/mail.py
@@ -1,6 +1,7 @@
 from flask_mail import Mail, Message
-
 from flask import current_app as app
+from .models import *
+import base64
 class Send_Mail:
 
     mails = []
@@ -20,8 +21,20 @@ class Send_Mail:
     #def __init__(self):
   
     def get_mails(self):
-        pass
-        
+        record = db.session.query(Encuestado).filter_by(activo=True).all()
+        for rec in record:
+            self.mails.append(rec.email)
+
+
+    def encode_link(self, str):
+        str_bytes = str.encode("ascii")
+        base_bytes = base64.b64encode(str_bytes)
+        return base_bytes.decode("ascii")
+
+    def decode_link(self, str):
+        base_bytes = str.encode("ascii")
+        str_bytes = base64.decode(base_bytes)
+        return str_bytes.decode("ascii")
 
     def send_mail(self):
 
@@ -30,9 +43,9 @@ class Send_Mail:
 
         with mail.connect() as conn:
 
-            for user in self.users:
+            for user in self.mails:
                 subject ="Saludos "+ user
-                message = "Hola "+ user +" Este es un el link de su encuesta: http://localhost:3000/encuesta1/"+user+"/"
+                message = "Hola "+ user +" Este es un el link de su encuesta: http://localhost:3000/encuesta1/"+ self.encode_link(user)+"/"
 
                 msg= Message(recipients=[user], body=message, subject=subject)
                 #print(user)
